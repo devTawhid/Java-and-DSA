e01b4724a2579a8a267b3101a502785c794f288a
devTawhid <thefreelancertawhid@gmail.com>
2021-09-21 11:08:03 +0600

    video - 8 done

--- src/com/arrays/arrayListMultidimentional.java
+++ src/com/arrays/arrayListMultidimentional.java
@@ -1,4 +1,27 @@
 package com.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class arrayListMultidimentional {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // initialization
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // add elements
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(in.nextInt());
+            }
+        }
+
+//        System.out.println(Arrays.toString(list));
+        System.out.println(list);
+    }
 }
---
+++ src/com/arrays/questions/Q1ArraySwap.java
@@ -0,0 +1,28 @@
+package com.arrays.questions;
+
+import java.util.Arrays;
+
+public class Q1ArraySwap {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+
+        System.out.println(Arrays.toString(arr));
+        swap(arr, 0, 1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    /***
+     *
+     * @param arr: the array the function will  run
+     * @param index1: the 1st index of the element need to be swppped
+     * @param index2: the 2nd index of the element need to be swppped
+     */
+
+    static void swap(int[] arr, int index1, int index2){
+        int temp;
+        temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
