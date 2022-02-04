3338c0386ccdf2773bd7104f7812cd9579f9110c
devTawhid <thefreelancertawhid@gmail.com>
2022-02-04 21:23:30 +0600

    Sorting the Sentence & Count Items Matching a RuleCount Items Matching a Rule

--- src/com/qString/p1773CountItemsMatchingaRule.java
+++ src/com/qString/p1773CountItemsMatchingaRule.java
@@ -1,17 +1,28 @@
 package com.qString;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class p1773CountItemsMatchingaRule {
     public static void main(String[] args) {
         List<List<String>> items = new ArrayList<>();
-//        items.add(new String[]{"phone", "blue", "pixel"}, {"computer","silver","phone"});
-//        [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]]
-//        countMatches()
+
+        items.add(Arrays.asList("phone", "blue", "pixel"));
+        items.add(Arrays.asList("computer","silver","phone"));
+        items.add(Arrays.asList("phone","gold","iphone"));
+
+        countMatches(items, "color", "silver");
     }
     static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int count = 0;
+        int eval = ruleKey.equals("type") ? 0: ruleKey.equals("color")? 1: 2; // this made the code way faster so remember this part.
+
+        for (List<String> item:items) {
+            if(item.get(eval).equals(ruleValue))
+                count++;
+        }
+        System.out.println(count);
         return count;
     }
 }
--- src/com/qString/p1859SortingtheSentence.java
+++ src/com/qString/p1859SortingtheSentence.java
@@ -1,12 +1,45 @@
 package com.qString;
 
+import java.util.Arrays;
+
 public class p1859SortingtheSentence {
     public static void main(String[] args) {
-        String s = "is2 sentence4 This1 a3";
-        sortSentence(s);
+        String s = "Myself2 Me1 I4 and3";
+        System.out.println(sortSentence(s));
     }
-    static String sortSentence(String s) {
 
-        return new String();
+    // 1st one
+    static String sortSentence(String s) {
+        String[] ans = new String[s.split(" ").length];
+
+        for (String str:s.split(" ")) {
+            ans[str.charAt(str.length() - 1) - '1'] = str.substring(0, str.length() - 1);
+        }
+
+        return String.join(" ", ans);
+    }
+
+    static String sortSentence_testing(String s) {
+        StringBuilder str = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+//            if(s.charAt(i) == ' ') {
+//                int num = i - 1;
+//                System.out.println(s.charAt(num));
+//            }if(i == s.length() - 1)
+//                System.out.println(s.charAt(i));
+//        }
+        String[] ans = new String[s.split(" ").length];
+        System.out.println(s.split(" ")[1].charAt(s.split(" ")[1].length() - 1) - '1');
+        System.out.println();
+        System.out.println();
+        for(String st: s.split(" ")){
+//            System.out.println(Arrays.toString(ans));
+            ans[st.charAt(st.length()-1) - '1'] = st.substring(0,st.length()-1);
+//            System.out.println(Arrays.toString(ans));
+//            System.out.println();
+        }
+        return String.join(" ", ans);
+//        System.out.println(s.split(" ").length);
+//        return str.toString();
     }
 }
